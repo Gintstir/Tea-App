5fe3831bef15205903e4402e387471402451741e
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-04-05 14:25:31 -0400

    basic app.js in /tea-app

--- tea-app/src/App.js
+++ tea-app/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React from "react";
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
+
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+import { Provider } from 'react-redux';
+import store from './utils/store';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql',
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          {/* adding StoreProvider in: */}
+          <Provider store={store}>
+            
+          </Provider>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
