f49bbc1ec4aecf8c7f127ff943f4fadd5d6c809f
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-04-05 14:42:18 -0400

    successful render

--- tea-app/src/App.js
+++ tea-app/src/App.js
@@ -1,39 +1,41 @@
 //import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import React from "react";
 //import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { BrowserRouter as Router} from "react-router-dom";
+// import { BrowserRouter as Router} from "react-router-dom";
 
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+// import { ApolloProvider } from '@apollo/react-hooks';
+// import ApolloClient from 'apollo-boost';
 
-import { Provider } from 'react-redux';
-import store from './utils/store';
+// import { Provider } from 'react-redux';
+// import store from './utils/store';
 
-const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem('id_token')
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    })
-  },
-  uri: '/graphql',
-})
+// const client = new ApolloClient({
+//   request: (operation) => {
+//     const token = localStorage.getItem('id_token')
+//     operation.setContext({
+//       headers: {
+//         authorization: token ? `Bearer ${token}` : ''
+//       }
+//     })
+//   },
+//   uri: '/graphql',
+// })
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-          {/* adding StoreProvider in: */}
-          <Provider store={store}>
+
+    <div>hello</div>
+    // <ApolloProvider client={client}>
+    //   <Router>
+    //     <div>
+    //       {/* adding StoreProvider in: */}
+    //       <Provider store={store}>
             
-          </Provider>
-        </div>
-      </Router>
-    </ApolloProvider>
+    //       </Provider>
+    //     </div>
+    //   </Router>
+    // </ApolloProvider>
   );
 }
 
