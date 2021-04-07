3a6aa61804f2fd14d4dab765449731975e20f043
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-04-07 18:06:23 -0400

    working in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,7 @@
 const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
+    
     type File {
         files: [String]
     }
@@ -13,6 +14,23 @@ const typeDefs = gql`
 
     type Mutation {
         loadImage(image: Upload!): Boolean!
+    }
+
+    type: User {
+        _id: ID
+        username: String
+        email: String
+        teas: [teaSchema]
+    }
+    type: Tea {
+        _id: ID
+        type: String
+        name: String
+        brand: String
+    }
+    type Extra {---------------------working here!
+        _id: ID
+
     }
 `
 
