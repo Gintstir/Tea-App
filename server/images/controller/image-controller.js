a6ee1d320631edfa0f39e9c7fbdd6b23cd837ad3
dalyd14 <dalyd14@gmail.com>
2021-04-07 07:46:35 -0400

    added file compression package jimp

--- server/images/controller/image-controller.js
+++ server/images/controller/image-controller.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+const jimp = require('jimp')
 
 const { Image } = require('../../models')
 
@@ -20,7 +21,18 @@ const imageController = {
         } else {
             res.status(500).json({ error: 'there was an error uploading this image'})
         }
+    },
+    async resizeImage (imgPath, width = 600, height = jimp.AUTO, quality = 70) {
+        const image = await jimp.read(imgPath)
+        await image.resize(width, height)
+        await image.quality(quality)
+        await image.writeAsync(imgPath)
     }
 }
+
+// for testing: make sure there is a test.jpg file in the images/images folder or you can just rename it
+// uncomment the command below
+// then run (depending where you are in the directory): node server/images/controller/image-controller
+// imageController.resizeImage(path.join(__dirname, '../images/test.JPG'), 600, undefined, 70)
 
 module.exports = imageController
