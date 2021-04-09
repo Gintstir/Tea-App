098d55d212781e695491267be22c35a0351b5e80
Michele Lee Lynch <michele.lynch8@yahoo.com>
2021-04-09 13:04:54 -0400

    added tea cards and brew timer

---
+++ tea-app/src/Tea/Tea.data.types.js
@@ -0,0 +1,19 @@
+
+
+export type id = string;
+export type tea = string;
+export type extra = string;
+export type temperature = string;
+export type steepTime = string;
+export type picture = string;
+export type note = string;
+export type brewTime = Array<any>;
+
+export type TEA_DATA = {
+  id: id,
+  image: image,
+  header: header,
+  text: text,
+  brewTime: brewTime
+};
+
