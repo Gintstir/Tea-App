9e4a8db6dcca143216083305abc6f02276e62e09
jyc5331 <jcosgrovecoding@gmail.com>
2021-04-11 16:31:41 -0400

    options for drop select window added to grommetsample to use with NewTea

--- tea-app/src/components/PantryShelfCard/index.js
+++ tea-app/src/components/PantryShelfCard/index.js
@@ -4,20 +4,20 @@ import React from "react";
 import { Grommet, Text, Card, CardBody } from "grommet";
 
 const PantryShelfCard = ({ cardData }) => {
-
-    return (
-        <Grommet>
-            <Card  height="xsmall" width="xsmall" background="light-1">
-                <CardBody fill="vertical" justify="center" align="center">
-                    {
-                        typeof(cardData) === 'string' ?
-                        <Text textAlign="center">{cardData}</Text> :
-                        <Text textAlign="center">{cardData.name}</Text>
-                    }
-                </CardBody>
-            </Card>
-        </Grommet>
-    );
-}
+  return (
+    <Grommet>
+      {/* make background equal to user's selection for teatype */}
+      <Card height="xsmall" width="xsmall" background="light-1">
+        <CardBody fill="vertical" justify="center" align="center">
+          {typeof cardData === "string" ? (
+            <Text textAlign="center">{cardData}</Text>
+          ) : (
+            <Text textAlign="center">{cardData.name}</Text>
+          )}
+        </CardBody>
+      </Card>
+    </Grommet>
+  );
+};
 
 export default PantryShelfCard;
