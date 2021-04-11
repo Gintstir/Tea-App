437473a7189863a027426f30988b32dee8a9733b
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-04-11 17:38:40 -0400

    stupid forms

--- tea-app/src/components/Register/index.js
+++ tea-app/src/components/Register/index.js
@@ -3,25 +3,14 @@ import { useMutation } from '@apollo/react-hooks';
 import Auth from '../../utils/auth';
 import { ADD_USER } from '../../utils/mutations';
 // import { grommet } from "grommet/themes";
-import { Grommet, Box, Form, FormField, TextInput, grommet, Button } from "grommet";
+import { Grommet, Box, Form, TextInput, grommet, Button } from "grommet";
 import { deepMerge } from 'grommet/utils';
 
 import { Link } from "react-router-dom";
 
 const customTheme = deepMerge(grommet, {
   global: {
-    colors: {
-      // Overriding existing grommet colors
-      brand: "#4D4CDB",
-      "accent-1": "#6FFFB0",
-      "accent-2": "#7FFFB0",
-      "accent-3": "#8FFFB0",
-      "accent-4": "#9FFFB0",
-      "neutral-1": "#10873D",
-      "neutral-2": "#20873D",
-      "neutral-3": "#30873D",
-      "neutral-4": "#40873D",
-      focus: "#000",
+    colors: {     
       // Setting new colors
       blue: "#00C8FF",
       green: "#749A5C",
@@ -29,53 +18,19 @@ const customTheme = deepMerge(grommet, {
       purple: "#A2065A",
       red: "#FC6161",
       orange: "#FFBC44",
-      yellow: "#FFEB59",
-      
-      // you can also point to existing grommet colors
-      brightGreen: "accent-1",
-      deepGreen: "neutral-2"
+      yellow: "#FFEB59",      
     }
   },
-  formField: {
-    border: {
-      error: {
-        color: 'border',
-      },
-      color: 'border',
-      side: 'all',
-    },
-    disabled: {
-      background: {
-        color: undefined,
-      },
-      border: {
-        color: 'status-disabled',
-      },
-      label: {
-        color: 'status-disabled',
-      },
-    },
-    error: {
-      background: {
-        color: { light: '#FF404033', dark: '#FF40404D' },
-      },
-      size: 'xsmall',
-      color: 'text-weak',
-      margin: {
-        start: 'none',
-      },
-    },
-      
-    label: {
-      size: 'medium',
-      color: 'purple',
-      margin: {
-        horizontal: 'none',
-      },
-      textAlign: 'center'
-    },
-    round: '8px',
-  }    
+  textInput: {
+   
+    //  background: lightblue;
+    //  font-size: 20px;
+    
+     
+    
+  },  
+  round: '8px',
+    
 });
 
 const Register = () => {
@@ -107,33 +62,34 @@ const Register = () => {
       <Box align="center" pad="large" >
         <h1>Sign Up!</h1>
         <Form onSubmit={handleFormSubmit} >
-            <Box border gap="medium" pad="large" width="medium" background="orange">
-                <FormField 
-                  htmlFor="username"
-                  name="username"
-                  type="username"
-                  label="Username"
-                  contentProps={{
-                    background: "lightblue"
-                  }}
-                  
-                  >
+            <Box fill gap="medium" pad="large" width="medium" background="orange">
+                <Box width="medium">                  
                     <TextInput 
+                      type="username"
                       id="username"
                       name="username"                              
-                      onChange={handleChange} 
-                      />
-                </FormField>
-                <FormField htmlFor="email" name="email" label="Email" contentProps={{
-                    background: "lightblue"
-                  }}>
-                    <TextInput id="email" name="email" type="email" onChange={handleChange}/>
-                </FormField>
-                <FormField htmlFor="password" name="password" label="Password" contentProps={{
-                    background: "lightblue"
-                  }}>
-                    <TextInput id="password" name="password"  type="password" onChange={handleChange}/>
-                </FormField>
+                      onChange={handleChange}
+                      placeholder={<span>Username</span>}                      
+                      />                
+                </Box>
+                <Box width="medium">                  
+                    <TextInput 
+                      type="email"
+                      id="email"
+                      name="email"                      
+                      onChange={handleChange}
+                      placeholder={<span>Email</span>}
+                      />                
+                </Box>
+                <Box width="medium">                  
+                    <TextInput
+                      type="password"
+                      id="password"
+                      name="password"                     
+                      onChange={handleChange}
+                      placeholder={<span>Password</span>}
+                      />                
+                </Box>
                 <Button type="submit" label="Welcome!" primary color="purple" />
             </Box>
         </Form>
