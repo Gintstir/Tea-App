57148b1d7b3ddfef14d279f8ad38917dc4b395b3
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-04-12 16:37:14 -0400

    tea form is a little neater

--- tea-app/src/components/NewRecipe/index.js
+++ tea-app/src/components/NewRecipe/index.js
@@ -1,118 +1,199 @@
-import React, { useState } from 'react'
-import { nanoid } from 'nanoid'
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
 
-import { Form, FormField, TextInput, Box, Button, Grommet, Text, CheckBoxGroup} from 'grommet'
-import { useMutation } from '@apollo/react-hooks'
+import {
+  Form,
+  FormField,
+  TextInput,
+  Box,
+  Button,
+  Grommet,
+  Text,
+  CheckBoxGroup,
+  grommet
 
-import Upload from '../Upload'
+} from "grommet";
+import { deepMerge } from 'grommet/utils';
+import { useMutation } from "@apollo/react-hooks";
 
-import { ADD_RECIPE, UPLOAD_IMAGE } from '../../utils/mutations'
+import Upload from "../Upload";
+
+import { ADD_RECIPE, UPLOAD_IMAGE } from "../../utils/mutations";
+
+const customTheme = deepMerge(grommet, {
+    global: {
+        input: {
+            padding: {
+                vertical: "20px"
+            }
+        },
+        colors: {
+            placeholder: "#A2065A"
+        }
+    }
+})
 
 const NewRecipe = ({ setShow }) => {
+  const [image, setImage] = useState();
 
-    const [image, setImage] = useState()
+  const [formValue, setFormValue] = useState({
+    type: "",
+    name: "",
+    brand: "",
+    extra: [],
+    temperature: "",
+    steepTime: "",
+    note: "",
+  });
 
-    const [formValue, setFormValue] = useState({
-        type: '',
-        name: '',
-        brand: '',
+  const [addRecipe, { error }] = useMutation(ADD_RECIPE);
+
+  const [uploadImage] = useMutation(UPLOAD_IMAGE);
+
+  const handleSubmit = (value) => {
+    // console.log(nanoid())
+    let imageName;
+    if (image) {
+      imageName = generateFileName(image?.name);
+    }
+
+    value.steepTime = parseInt(value.steepTime);
+
+    Promise.all([
+      addRecipe({ variables: { ...value, picture: imageName } }),
+      uploadImage({ variables: { image, imageName } }),
+    ]).then((values) => {
+      setFormValue({
+        type: "",
+        name: "",
+        brand: "",
         extra: [],
-        temperature: '',
-        steepTime: '',
-        note: ''
-    })
-    
-    const [addRecipe, { error }] = useMutation(ADD_RECIPE)
+        temperature: "",
+        steepTime: "",
+        note: "",
+      });
 
-    const [uploadImage] = useMutation(UPLOAD_IMAGE)
+      setShow(false);
+    });
+  };
 
-    const handleSubmit = (value) => {
-        
-        // console.log(nanoid())
-        let imageName
-        if (image) {
-            imageName = generateFileName(image?.name)
-        }
+  const generateFileName = (oldFilename) => {
+    let fileExt = oldFilename.split(".");
+    fileExt = fileExt[fileExt.length - 1];
+    return nanoid() + "." + fileExt;
+  };
 
-        value.steepTime = parseInt(value.steepTime)
-        
-        Promise.all([
-            addRecipe({ variables: { ...value, picture: imageName} }),
-            uploadImage({ variables: { image, imageName }})
-        ]).then(values => {
+
+
+  return (
+    <Grommet theme={customTheme}>
+      <Box justify="center">
+        <Form
+          value={formValue}
+          onChange={(nextValue) => setFormValue(nextValue)}
+          onReset={() =>
             setFormValue({
-                type: '',
-                name: '',
-                brand: '',
-                extra: [],
-                temperature: '',
-                steepTime: '',
-                note: ''
+              type: "",
+              name: "",
+              brand: "",
+              extra: [],
+              temperature: "",
+              steepTime: "",
+              note: "",
             })
-
-            setShow(false)
-        })
-    }
-
-    const generateFileName = (oldFilename) => {
-        let fileExt = oldFilename.split('.')
-        fileExt = fileExt[fileExt.length - 1]
-        return nanoid() + '.' + fileExt
-    }
-    
-
-
-
-    return (
-        <Grommet>
-            <Box justify="center">
-                <Form
-                    value={formValue}
-                    onChange={nextValue => setFormValue(nextValue)}
-                    onReset={() => setFormValue({
-                        type: '',
-                        name: '',
-                        brand: '',
-                        extra: [],
-                        temperature: '',
-                        steepTime: '',
-                        note: ''
-                    })}
-                    onSubmit={ async ({value}) => handleSubmit(value) }
-                >
-                    <FormField name="type" htmlFor="tea-type-id" label="Type">
-                        <TextInput type="text" id="tea-type-id" name="type" />
-                    </FormField>
-                    <FormField name="name" htmlFor="tea-name-id" label="Name">
-                        <TextInput type="text" id="tea-name-id" name="name" />
-                    </FormField>
-                    <FormField name="brand" htmlFor="tea-brand-id" label="Brand">
-                        <TextInput type="text" id="tea-brand-id" name="brand" />
-                    </FormField>
-                    <FormField name="extra" htmlFor="tea-extra-id" label="Extra">
-                        <CheckBoxGroup options={["Milk", "Sugar", "Honey"]} id="tea-extra-id" name="extra" />
-                    </FormField>
-                    <FormField name="temperature" htmlFor="tea-temperature-id" label="Temperature">
-                        <TextInput type="text" id="tea-temperature-id" name="temperature" />
-                    </FormField>
-                    <FormField name="steepTime" htmlFor="tea-steepTime-id" label="Steep Time">
-                        <TextInput type="number" id="tea-steepTime-id" name="steepTime" />
-                    </FormField>
-                    <FormField type="text" name="note" htmlFor="tea-note-id" label="Note">
-                        <TextInput type="text" id="tea-note-id" name="note" />
-                    </FormField>
-                    <FormField name="image" htmlFor="tea-image-id" label="Picture">
-                        <Upload setImage={setImage} />
-                    </FormField>
-                    <Box direction="row" gap="medium"  justify="center">
-                        <Button type="submit" primary label="Submit" />
-                        <Button type="reset" label="Reset" />
-                    </Box>
-                </Form>
-                { error && <Text>{error.message}</Text>}    
+          }
+          onSubmit={async ({ value }) => handleSubmit(value)}
+        >
+          <Box align="center">
+            {/* <FormField name="type" htmlFor="tea-type-id" label="Type"> */}
+            <TextInput
+              textAlign="center"
+              pad="medium"
+              type="text"
+              id="tea-type-id"
+              name="type"
+              placeholder="Tea Type"
+            />
+            {/* </FormField> */}
+            {/* <FormField name="name" htmlFor="tea-name-id" label="Name"> */}
+            <TextInput
+              textAlign="center"
+              pad="medium"
+              plain
+              type="text"
+              id="tea-name-id"
+              name="name"
+              placeholder="Tea Name"
+            />
+            {/* </FormField> */}
+            {/* <FormField name="brand" htmlFor="tea-brand-id" label="Brand"> */}
+            <TextInput
+              textAlign="center"
+              type="text"
+              id="tea-brand-id"
+              name="brand"
+              placeholder="Tea Brand"
+            />
+            {/* </FormField> */}
+            {/* <FormField name="extra" htmlFor="tea-extra-id" label="Extra"> */}
+            <CheckBoxGroup
+              options={["Milk", "Sugar", "Honey"]}
+              id="tea-extra-id"
+              name="extra"
+            />
+            {/* </FormField> */}
+            {/* <FormField
+              name="temperature"
+              htmlFor="tea-temperature-id"
+              label="Temperature"
+            > */}
+              <TextInput
+                textAlign="center"
+                type="text"
+                id="tea-temperature-id"
+                name="temperature"
+                placeholder="Brewing Temperature (℉)"
+              />
+            {/* </FormField> */}
+            {/* <FormField
+              name="steepTime"
+              htmlFor="tea-steepTime-id"
+              label="Steep Time"
+            > */}
+              <TextInput
+                textAlign="center"
+                type="number"
+                id="tea-steepTime-id"
+                name="steepTime"
+                placeholder="Steep Time (minutes)" />
+            {/* </FormField> */}
+            {/* <FormField
+              type="text"
+              name="note"
+              htmlFor="tea-note-id"
+              label="Note"
+            > */}
+              <TextInput 
+                textAlign="center"
+                type="text"
+                id="tea-note-id"
+                name="note"
+                placeholder="Note"
+                 />
+            {/* </FormField> */}
+            <FormField name="image" htmlFor="tea-image-id" label="Picture">
+              <Upload setImage={setImage} />
+            </FormField>
+            <Box direction="row" gap="medium" justify="center">
+              <Button type="submit" primary label="Submit" />
+              <Button type="reset" label="Reset" />
             </Box>
-        </Grommet>
-    )
-}
+          </Box>
+        </Form>
+        {error && <Text>{error.message}</Text>}
+      </Box>
+    </Grommet>
+  );
+};
 
-export default NewRecipe
+export default NewRecipe;
