adbb73f559a458524d5d86e7bbb88f949c51c76d
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-04-12 11:56:31 -0400

    added icons to manifest.json

--- tea-app/src/index.js
+++ tea-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from '../public/serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,4 +20,4 @@ reportWebVitals();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
+serviceWorker.register();
