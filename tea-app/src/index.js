fc0a6c3c0849335030908bc3f86daa7d6142dbf6
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-04-12 12:07:43 -0400

    moved serviceWorker.js to /public

--- tea-app/src/index.js
+++ tea-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from '../public/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
