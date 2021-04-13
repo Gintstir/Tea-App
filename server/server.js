7dcac890786f541a723b84c6d126e47aab81f884
dalyd14 <dalyd14@gmail.com>
2021-04-12 22:45:53 -0400

    hopefully succesffully added https redirect

--- server/server.js
+++ server/server.js
@@ -29,6 +29,12 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "./images")));
 
 if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
   app.use(express.static(path.join(__dirname, "../tea-app/build")));
 }
 
